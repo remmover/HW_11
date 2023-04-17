33b57d93f4e376405ae9ee2b799812a8cb347870
Ihor Moroz <igorm.200323@gmail.com>
2023-04-17 14:35:08 +0300

    second commit
    fixing critical errors

--- phone_bot_v03.py
+++ phone_bot_v03.py
@@ -1,30 +1,28 @@
+import re
 from collections import UserDict
+from datetime import datetime
 from functools import wraps
+from itertools import islice
+
 
 
 class Field:
     def __init__(self, value):
         self.value = value
 
-    def __setitem__(self):
-        pass
-
-    def __getter__(self):
-        pass
-
     def __str__(self):
         return str(self.value)
 
 
-class Record(Field):
-    def __init__(self, name, phone=None, date=None):
+class Record:
+    def __init__(self, name, phone=None, b_date=None):
         self.name = name
         self.phone = phone
         self.phones = [phone] if phone else []
-        self.days = date
+        self.b_date = b_date
 
     def add_phone(self, phone):
-        self.phones.append(phone)
+        self.phones.append(f'num = {phone}')
 
     def change_phone(self, name, phone):
         self.phones[name] = phone
@@ -32,18 +30,53 @@ class Record(Field):
     def delete_phone(self, phone):
         self.phones.remove(phone)
 
-    def days_to_birthday(self, date):
-        pass
+    def days_to_birthday(self, b_date):
+        b_date = str(b_date)
+        current_date = datetime.now().date()
+        normal_date = datetime.strptime(b_date, '%d.%m.%Y').replace(year=current_date.year).date()
+        days = (normal_date - current_date).days
+        if  days < 0:
+            normal_date = datetime.strptime(b_date, '%d.%m.%Y').replace(year=current_date.year + 1).date()
+            days = (normal_date - current_date).days
+        self.phones.append(f'days to BD = {days} ;')
 
+    def __repr__(self) -> str:
+        return f"{', '.join([str(i) for i in self.phones])}"
+        
 
-class Birthday:
-    def __setitem__(self):
-        pass
+class Birthday(Field):
+    def __init__(self, value=None):
+        self.__value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, bd_date):
+        try:
+            datetime.strptime(bd_date, '%d.%m.%Y')
+        except ValueError:
+            raise ValueError("Birthdate must be in 'dd.mm.yyyy' format")
+        self.__value = bd_date
 
 
 class Phone(Field):
-    def __setitem__(self):
-        pass
+
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, phone):
+        if not re.match(r"^(?:\+380|0)(?:39|67|68|96|97|98|50|66|95|99|63|73| 93)\d{7}$", phone):
+            raise ValueError("Phone must start with +380 or 0 and has 9 nums after")
+        self.__value = phone
 
 
 class Name(Field):
@@ -55,9 +88,15 @@ class AddressBook(UserDict):
         name = record.name.value
         self.data[name] = record
 
-    def iterator(self):
-        pass
-
+    def iterator(self, records_count = 3):
+        start_iterate = 0
+        while True:
+            if start_iterate >= len(self.data):
+                break
+            yield dict(islice(self.data.items(),
+                              start_iterate, 
+                              start_iterate + records_count))
+            start_iterate += records_count
 
 contacts = AddressBook()
 
@@ -89,13 +128,23 @@ def hello(*args):
 @input_error
 def add_func(*args):
     obj = args[0].split()
+    if len(obj) < 2 or len(obj) > 3:
+        raise ValueError(
+            "Invalid input format. Expected 'Name Phone [Birthday]'")
+
     name = Name(obj[0])
     phone = Phone(obj[1])
     record = Record(name)
     record.add_phone(phone)
-    contacts.add_record(record)
-    return f'U add contact with name : {name} and his number is : {phone}'
-
+    if len(obj) == 3:
+        bd_date = Birthday(obj[2])
+        record.days_to_birthday(bd_date)
+        contacts.add_record(record)
+        return f'U add contact with all parameters : {name} and his number is : {phone} and his BD : {bd_date}'
+    else:
+        contacts.add_record(record)
+        return f'U add contact with name: {name} and his number is : {phone}'
+    
 
 @input_error
 def change_contacts_dict(*args):
@@ -119,14 +168,14 @@ def phone_func(*args):
 
 
 def show_all_func(*args):
-    list = []
     if len(contacts.data) == 0:
         return "list of contacts is empty..."
-    for key in contacts.data.keys():
-        record = contacts.data[key]
-        list.append(
-            f"{key}: {', '.join(str(phone) for phone in record.phones)}")
-    return "\n".join([f"{item}"for item in list])
+    records_count = args[0]
+    if records_count:
+        for i in contacts.iterator(int(records_count)):
+            print(i)
+    else:
+        return contacts
 
 
 def no_command(*args):
@@ -138,9 +187,10 @@ OPERATION = {
     'add': add_func,
     'change': change_contacts_dict,
     'phone': phone_func,
-    'show': show_all_func,
+    'show all': show_all_func,
     'help': help
 }
+
 
 END_WORDS = ['good bye', 'close', 'exit']
 
@@ -165,3 +215,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+     
